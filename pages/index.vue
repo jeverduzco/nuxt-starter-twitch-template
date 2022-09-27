43f04e7d735390df36717c6a5bfcc021211a2ca5
Jesus Verduzco <jesus@verduzco.me>
2022-09-27 18:25:05 -0500

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="hi">
+      Hola Twitch
+    </div>
+  </div>
+</template>
+<style scoped>
+  .hi {
+    height: calc(100vh - 60px);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
